206b0848010de5b13dae170669368971fa3ad296
fqjupe <jukka.pelli@tuni.fi>
2022-09-23 13:34:09 +0300

    Created some dummyclasses for project structure

---
+++ Group3/src/main/java/fi/tuni/swdesign/group3/Main.java
@@ -0,0 +1,11 @@
+package fi.tuni.swdesign.group3;
+
+/**
+ *
+ * Placeholder for main
+ */
+public class Main {
+    public static void main(String args[]){
+    System.out.println("Just a dummy class");
+    }
+}
---
+++ Group3/src/test/java/fi/tuni/swdesign/group3/MainTest.java
@@ -0,0 +1,11 @@
+package fi.tuni.swdesign.group3;
+
+
+/*
+* Placeholder for initial tests
+*/
+public class MainTest {
+    
+    public MainTest() {
+    }
+}
