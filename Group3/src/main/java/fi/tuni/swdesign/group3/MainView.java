0c139e6e0e4410f16515bf9426f6366d48213c4f
unknown <lauri.puoskari@tuni.fi>
2022-09-23 15:55:03 +0300

    Added empty View classes and added dependencies in pom.xml

---
+++ Group3/src/main/java/fi/tuni/swdesign/group3/MainView.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.swdesign.group3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Lauri Puoskari
+ */
+public class MainView extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ Group3/src/main/java/fi/tuni/swdesign/group3/WeatherDataView.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.swdesign.group3;
+
+/**
+ *
+ * @author Lauri Puoskari
+ */
+public class WeatherDataView {
+    
+}
