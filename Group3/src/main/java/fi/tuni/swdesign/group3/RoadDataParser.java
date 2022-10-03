cdb8deda2c985cd9927766cf43ab3d3bb2a9befb
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-10-04 00:44:45 +0300

    added function readRoadConditions(), bugfixes in readMaintenanceTasks()

--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataParser.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataParser.java
@@ -55,13 +55,13 @@ public class RoadDataParser {
      
     }
     
-    private static void ReadMaintenanceTasks(String minX, String maxX,String minY, String maxY) throws MalformedURLException, IOException{
+    private static void readMaintenanceTasks(String minX, String maxX,String minY, String maxY) throws MalformedURLException, IOException{
         
         String sUrl = "https://tie.digitraffic.fi/api/maintenance/v1/tracking/routes/latest?";        
-        String coordinates = "xMin="+minX+"&yMin="+minY+"&xMax"+maxX+"&yMax"+"&yMax"+maxY; 
+        String coordinates = "xMin="+minX+"&yMin="+minY+"&xMax"+maxX+"&yMax"+maxY; 
         String fullUrl = sUrl+coordinates+"&domain=state-roads";
         
-        URL url = new URL(sUrl);
+        URL url = new URL(fullUrl);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("accept", "*/*");
@@ -80,17 +80,56 @@ public class RoadDataParser {
             String id = properties.get("id").getAsString();
             JsonArray taskNames = properties.getAsJsonArray("tasks");
             
-            ArrayList<String> tasks = null;
+            ArrayList<String> tasks = new ArrayList<String>();
             for(var inner : taskNames){
-                String test = inner.getAsString();
-                tasks.add(test);
+                String taskName = inner.getAsString();
+                tasks.add(taskName);
             }
         }
         
     }
     
+    public static void readRoadConditions(String minX, String maxX, String minY, String maxY) throws MalformedURLException, IOException{
+        
+        String sUrl = "https://tie.digitraffic.fi/api/v3/data/road-conditions/";        
+        String coordinates = minX+"/"+minY+"/"+maxX+"/"+maxY; 
+        String fullUrl = sUrl+coordinates;
+        
+        URL url = new URL(fullUrl);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("accept", "*/*");
+        con.setRequestProperty("Accept-Encoding", "gzip");
+        
+        GZIPInputStream gzipInput = new GZIPInputStream(con.getInputStream());
+        InputStreamReader reader = new InputStreamReader(gzipInput, "UTF-8");
+        
+        Gson gson = new Gson();
+        JsonObject response = gson.fromJson(reader, JsonObject.class);
+        JsonArray weatherData = response.getAsJsonArray("weatherData");
+        
+        for(var data: weatherData){
+            JsonObject forecast = (JsonObject) data;
+            JsonArray roadConditions = forecast.getAsJsonArray("roadConditions");
+            for(var road : roadConditions){
+                JsonObject condition = road.getAsJsonObject();
+                String time = condition.get("forecastName").getAsString();
+                String overallRoadCondition = condition.get("overallRoadCondition").getAsString();
+                String weatherSymbol = condition.get("weatherSymbol").getAsString();
+
+                if(condition.get("type").getAsString() != "OBSERVATION"){
+                    JsonObject forecastCondition = condition.getAsJsonObject("forecastConditionReason");
+
+                    forecastCondition.get("precipitationCondition").getAsString();
+                    forecastCondition.get("roadCondition").getAsString();
+
+                }
+            }
+        }
+    }
+    
     public static void main(String args[]) throws IOException {
         // TODO code application logic here
-        ReadMaintenanceTasks("19","32","59","72");
+        readRoadConditions("19","32","59","72");
     }
 }
