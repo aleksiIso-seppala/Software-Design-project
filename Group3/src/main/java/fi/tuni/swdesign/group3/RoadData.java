9ec097315f268c5f948d7cecec9f92f9f4f38d83
fqjupe <jukka.pelli@tuni.fi>
2022-10-07 13:38:11 +0300

    Added basic structure for roadData abstract class and its extending under modules

---
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadData.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.swdesign.group3;
+
+/**
+ *
+ * @author jukka
+ * An abstract class of RoadData. Extended by different under-modules.
+ */
+abstract class RoadData {
+    protected String location;
+    protected String coordinates;
+    
+    /*
+    * Default constructor for a RoadData
+    * @param location The location of the data as in place/town/city
+    * @param coordinates The coordinates of the location
+    */
+    public RoadData(String location, String coordinates){
+        this.location = location;
+        this.coordinates = coordinates;
+    }
+    
+    @Override
+    public String toString(){
+        return this.location+" - "+this.coordinates;
+    }
+    
+    /*
+    * Setter for location
+    */
+    public void setLocation(String location){
+        this.location = location;
+    }
+    
+    /*
+    * Setter for coordinates
+    */
+    public void setCoordinates(String coordinates){
+        this.coordinates = coordinates;
+    }
+    
+    /*
+    * Getter for location
+    * @return the location as a string
+    */
+    public String getLocation(){
+        return this.location;
+    }
+    
+    /*
+    * Getter for coordinates
+    * @return the coordinates as a string
+    */
+    public String getCoordinates(){
+        return this.coordinates;
+    }
+}
---
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadWeatherData.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.swdesign.group3;
+
+/**
+ *
+ * @author jukka
+ */
+public class RoadWeatherData extends RoadData{
+
+    public RoadWeatherData(String location, String coordinates){
+        super(location, coordinates);
+    }
+}
