d3bd6161a3c35bfb43c6eb08e944135548e64fc6
fqjupe <jukka.pelli@tuni.fi>
2022-10-28 11:52:44 +0300

    error corrections to UML and updated roadWeatherData with setters and getters

--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadWeatherData.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadWeatherData.java
@@ -4,13 +4,55 @@
  */
 package fi.tuni.swdesign.group3;
 
+import java.util.HashMap;
+
 /**
  *
  * @author jukka
  */
 public class RoadWeatherData extends RoadData{
+    private int temperature;
+    private int wind;
+    private String cloudiness;
+
+    //String is the time (2, 4, 6 etc) and then the forecast
+    // forecasts are the same as the observed so no need to do a new
+    // class for them
+    private HashMap<String, RoadWeatherData> forecasts;
 
     public RoadWeatherData(String location, String coordinates, String time){
         super(location, coordinates, time);
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getWind() {
+        return wind;
+    }
+
+    public void setWind(int wind) {
+        this.wind = wind;
+    }
+
+    public String getCloudiness() {
+        return cloudiness;
+    }
+
+    public void setCloudiness(String cloudiness) {
+        this.cloudiness = cloudiness;
+    }
+
+    public HashMap<String, RoadWeatherData> getForecasts() {
+        return forecasts;
+    }
+
+    public void setForecasts(HashMap<String, RoadWeatherData> forecasts) {
+        this.forecasts = forecasts;
     }
 }
