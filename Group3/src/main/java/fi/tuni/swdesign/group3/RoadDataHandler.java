ca0080c0e411e64bb376f1af7d80c4d908c8b676
fqjupe <jukka.pelli@tuni.fi>
2022-11-14 18:56:13 +0200

    advanced the data fetching with the handler, added default locations with HashMap

--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataHandler.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataHandler.java
@@ -4,8 +4,8 @@
  */
 package fi.tuni.swdesign.group3;
 
+import java.util.List;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import com.google.gson.JsonObject;
 import java.io.IOException;
@@ -16,23 +16,60 @@ import java.io.IOException;
  */
 public class RoadDataHandler {
     private HashMap<String, HashMap<String, ArrayList<RoadData>>> database;
+    private HashMap<String, ArrayList<String>> locations;
     
     RoadDataHandler(){
         database = new HashMap<>();
+        locations = new HashMap<>();
+        locations.put("Suomi", new ArrayList<>(List.of("19", "32", "59", "72")));
+        locations.put("Helsinki", new ArrayList<>(List.of()));
+        locations.put("Kuopio", new ArrayList<>(List.of()));
+        locations.put("Oulu", new ArrayList<>(List.of()));
+        locations.put("Tampere", new ArrayList<>(List.of()));
+        locations.put("Rovaniemi", new ArrayList<>(List.of()));
     }
     
-    public void fetchData(){
+    public RoadTrafficData fetchData(String location){
         try {
-            JsonObject roadConditions = RoadDataGetter.getRoadConditionData("Suomi","19","32","59","72");
+            
+            ArrayList loc = this.locations.get(location);
+            
+            JsonObject roadConditions = RoadDataGetter.
+                    getRoadConditionData(location, loc.get(0).toString(),
+                            loc.get(1).toString(), loc.get(2).toString(),
+                            loc.get(3).toString());
                      
-            JsonObject maintenanceTasks = RoadDataGetter.getMaintenanceTaskData("19","32","59","72");
+            JsonObject maintenanceTasks = RoadDataGetter.
+                    getMaintenanceTaskData(loc.get(0).toString(),
+                            loc.get(1).toString(), loc.get(2).toString(),
+                            loc.get(3).toString());
             
-            RoadTrafficData data = RoadDataParser.getRoadData("Suomi","19","32","59","72", roadConditions, maintenanceTasks);
+            RoadTrafficData data = RoadDataParser.
+                    getRoadData(location, loc.get(0).toString(),
+                            loc.get(1).toString(), loc.get(2).toString(),
+                            loc.get(3).toString(), roadConditions, 
+                            maintenanceTasks);
             
-            System.out.print("windspeed:"+data.getWindSpeed()+" ; @"+data.getCoordinates());
+            return data;
             
         } catch (IOException e){
             System.out.print("error");
+            return null;
+        }
+    }
+    
+    public RoadTrafficData fetchData(String location, String minX, String maxX, String minY, String maxY){
+        try {
+            JsonObject roadCond = RoadDataGetter.getRoadConditionData(location, minX, maxX, minY, maxY);
+            JsonObject maintTasks = RoadDataGetter.getMaintenanceTaskData(minX, maxX, minY, maxY);
+            
+            RoadTrafficData data = RoadDataParser.getRoadData(location, minX, maxX, minY, maxY, roadCond, maintTasks);
+            
+            return data;
+            
+        } catch (IOException e){
+            System.out.println("error");
+            return null;
         }
     }
     
@@ -66,6 +103,14 @@ public class RoadDataHandler {
             }
         }
         return null;
+    }
+    
+    public void saveDataBase(){
+        
+    }
+    
+    public void loadDataBase(){
+        
     }
     
 }
