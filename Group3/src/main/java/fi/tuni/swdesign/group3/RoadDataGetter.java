9720013d83629ffc06109a3daa45a69459b878d0
Aleksi <aleksi.iso-seppala@tuni.fi>
2022-11-15 12:23:05 +0200

    added functions getTrafficMessageData for getting every type of message data for easier usage in the handler

--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataGetter.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataGetter.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
 package fi.tuni.swdesign.group3;
-
+import fi.tuni.swdesign.group3.view.*;
+import fi.tuni.swdesign.group3.RoadDataParser;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -12,6 +13,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.zip.GZIPInputStream;
 /**
  *
@@ -35,10 +37,8 @@ public class RoadDataGetter {
             gzipInput = new GZIPInputStream(con.getInputStream());
         }
         catch (IOException ex){
-            int rCode = con.getResponseCode();
-            if(rCode != 200){
-                System.out.println("Something went wrong");
-            }             
+            System.out.println("Something went wrong");
+            return null;
         }
         InputStreamReader reader = new InputStreamReader(gzipInput, "UTF-8");
         
@@ -118,8 +118,56 @@ public class RoadDataGetter {
         return response;
     }
     
+    public static ArrayList<JsonObject> getTrafficMessageData() throws MalformedURLException, IOException{
+        
+        ArrayList<JsonObject> messageData = new ArrayList<>();
+        
+        String sUrl = "https://tie.digitraffic.fi/api/traffic-message/v1/messages?"
+                + "inactiveHours=0&includeAreaGeometry=true&situationType=";
+        
+        ArrayList<String> situationTypes = new ArrayList<>();
+        situationTypes.add("TRAFFIC_ANNOUNCEMENT");
+        situationTypes.add("EXEMPTED_TRANSPORT");
+        situationTypes.add("WEIGHT_RESTRICTION");
+        situationTypes.add("ROAD_WORK");
+        
+        for(var situation : situationTypes){
+            String fullUrl = sUrl+situation;
+            URL url = new URL(fullUrl);
+            
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("accept", "*/*");
+            con.setRequestProperty("Accept-Encoding", "gzip");
+
+            GZIPInputStream gzipInput = null;
+            try {
+                gzipInput = new GZIPInputStream(con.getInputStream());
+            } catch (IOException ex) {
+                continue;
+
+            }
+
+            InputStreamReader reader = new InputStreamReader(gzipInput, "UTF-8");
+
+            Gson gson = new Gson();
+            JsonObject response = gson.fromJson(reader, JsonObject.class);
+            messageData.add(response);
+        }
+        
+        if(messageData.size() == 0){
+            System.out.println("No traffic messages.");
+            return null;
+        }
+        
+        return messageData;
+    }
+    
     public static void main(String args[]) throws IOException {
         // TODO code application logic here
-        getMaintenanceTaskData("19","32","59","72");
+        // var response = getRoadConditionData("suomi","100","100","100","100");
+        // RoadDataParser.readFirstCondition("suomi", "19", "32", "59", "72", response);
+        int messages = RoadDataParser.readTrafficMessages(getTrafficMessageData());
+        System.out.println(messages);
     }
 }
