229f50e505a2770ed64abdcec00886fad93bfa61
Amanda Dieuaide <amanda.dieuaide@tuni.fi>
2022-11-17 02:32:48 +0200

    Changed FMI parsing and RoadWeatherData model - Data and forecasts are stored in RoadWeatherData Objects

--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataParserXML.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataParserXML.java
@@ -16,21 +16,4 @@ import java.util.ArrayList;
  * @author amand
  */
 public class RoadDataParserXML {
-    /*public static RoadWeatherData getWeatherData(String location, String minX, String maxX,
-            String minY, String maxY, JsonObject condition, JsonObject tasks, ArrayList<JsonObject> messageData) throws IOException{
-        
-        var roadData = readFirstCondition(location, minX, maxX, minY, maxY, condition);
-        
-        if(tasks != null){
-            var maintenanceTasks = readMaintenanceTasks(tasks);
-            roadData.setMaintenanceTasks(maintenanceTasks);
-        }
-        if(messageData != null){
-            int messages = readTrafficMessages(messageData);
-            roadData.setNumberOfTrafficMessages(messages);            
-        }
-
-        
-        return roadData;
-    }*/
 }
