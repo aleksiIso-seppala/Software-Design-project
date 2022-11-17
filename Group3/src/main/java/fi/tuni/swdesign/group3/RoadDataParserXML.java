6a8be53c69e54423687af31a4e268654067d2fd3
Amanda Dieuaide <amanda.dieuaide@tuni.fi>
2022-11-17 02:31:53 +0200

    Changed FMI parsing and RoadWeatherData model - Data and forecasts are stored in RoadWeatherData Objects

--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataParserXML.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataParserXML.java
@@ -4,10 +4,33 @@
  */
 package fi.tuni.swdesign.group3;
 
+import com.google.gson.JsonObject;
+import static fi.tuni.swdesign.group3.RoadDataParser.readFirstCondition;
+import static fi.tuni.swdesign.group3.RoadDataParser.readMaintenanceTasks;
+import static fi.tuni.swdesign.group3.RoadDataParser.readTrafficMessages;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author amand
  */
 public class RoadDataParserXML {
-    
+    /*public static RoadWeatherData getWeatherData(String location, String minX, String maxX,
+            String minY, String maxY, JsonObject condition, JsonObject tasks, ArrayList<JsonObject> messageData) throws IOException{
+        
+        var roadData = readFirstCondition(location, minX, maxX, minY, maxY, condition);
+        
+        if(tasks != null){
+            var maintenanceTasks = readMaintenanceTasks(tasks);
+            roadData.setMaintenanceTasks(maintenanceTasks);
+        }
+        if(messageData != null){
+            int messages = readTrafficMessages(messageData);
+            roadData.setNumberOfTrafficMessages(messages);            
+        }
+
+        
+        return roadData;
+    }*/
 }
--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadWeatherData.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadWeatherData.java
@@ -11,9 +11,12 @@ import java.util.HashMap;
  * @author jukka
  */
 public class RoadWeatherData extends RoadData{
-    private int temperature;
-    private int wind;
-    private String cloudiness;
+    private float temperature;
+    private float wind;
+    private float cloudiness;
+    private float AVGTemperature;
+    private float MAXTemperature;
+    private float MINTemperature;
 
     //String is the time (2, 4, 6 etc) and then the forecast
     // forecasts are the same as the observed so no need to do a new
@@ -24,27 +27,27 @@ public class RoadWeatherData extends RoadData{
         super(location, coordinates, time);
     }
 
-    public int getTemperature() {
+    public float getTemperature() {
         return temperature;
     }
 
-    public void setTemperature(int temperature) {
+    public void setTemperature(float temperature) {
         this.temperature = temperature;
     }
 
-    public int getWind() {
+    public float getWind() {
         return wind;
     }
 
-    public void setWind(int wind) {
+    public void setWind(float wind) {
         this.wind = wind;
     }
 
-    public String getCloudiness() {
+    public float getCloudiness() {
         return cloudiness;
     }
 
-    public void setCloudiness(String cloudiness) {
+    public void setCloudiness(float cloudiness) {
         this.cloudiness = cloudiness;
     }
 
@@ -54,5 +57,34 @@ public class RoadWeatherData extends RoadData{
 
     public void setForecasts(HashMap<String, RoadWeatherData> forecasts) {
         this.forecasts = forecasts;
+    }
+
+    public float getAVGTemperature() {
+        return AVGTemperature;
+    }
+
+    public void setAVGTemperature(float AVGTemperature) {
+        this.AVGTemperature = AVGTemperature;
+    }
+
+    public float getMAXTemperature() {
+        return MAXTemperature;
+    }
+
+    public void setMAXTemperature(float MAXTemperature) {
+        this.MAXTemperature = MAXTemperature;
+    }
+
+    public float getMINTemperature() {
+        return MINTemperature;
+    }
+
+    public void setMINTemperature(float MINTemperature) {
+        this.MINTemperature = MINTemperature;
+    }
+
+    @Override
+    public String toString() {
+        return "RoadWeatherData{" + "location=" + location + ", time=" + time + ", temperature=" + temperature + ", wind=" + wind + ", cloudiness=" + cloudiness + ", AVGTemperature=" + AVGTemperature + ", MAXTemperature=" + MAXTemperature + ", MINTemperature=" + MINTemperature + '}';
     }
 }
