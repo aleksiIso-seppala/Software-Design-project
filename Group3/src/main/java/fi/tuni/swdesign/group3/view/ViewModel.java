37ac08610a1b8eccc4e6cc2a7705a32bc97ecca8
fqjupe <jukka.pelli@tuni.fi>
2022-11-23 11:49:51 +0200

    Added basic implementation of getting data after event in view, updating needs to be implemented

--- Group3/src/main/java/fi/tuni/swdesign/group3/view/ViewModel.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/view/ViewModel.java
@@ -1,4 +1,7 @@
 package fi.tuni.swdesign.group3.view;
+import fi.tuni.swdesign.group3.*;
+import java.util.HashMap;
+import javafx.event.ActionEvent;
 
 /**
  *
@@ -6,6 +9,8 @@ package fi.tuni.swdesign.group3.view;
  */
 public class ViewModel {
     private final Model model;
+    private RoadTrafficData trafficData;
+    private RoadWeatherData weatherData;
     
     /**
      * Default constructor for a viewModel
@@ -13,6 +18,22 @@ public class ViewModel {
      */
     ViewModel(Model m){
         this.model = m;
+    }
+            
+    public void onRoadDataCalculateButton(ActionEvent e, HashMap<String, String> parameters){
+        RoadTrafficData data = model.getRoadTrafficData(parameters.get("location"));
+    }
+    
+    public void onWeatherDataCalculateButton(ActionEvent e, HashMap<String, String> parameters){
+        RoadWeatherData data = model.getRoadWeatherData(parameters.get("location"), 
+                parameters.get("time"), parameters.get("futuretime"));
+    }
+    
+    public void onCombinedDataCalculateButton(ActionEvent e, HashMap<String, String> parameters){
+        RoadTrafficData rdata = model.getRoadTrafficData(parameters.get("location"));
+        RoadWeatherData wdata = model.getRoadWeatherData(parameters.get("location"), 
+                parameters.get("time"), parameters.get("futuretime"));
+                
     }
     
     
