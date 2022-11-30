475d1a184e6903ed1cbc13941cdbc510d53157de
fqjupe <jukka.pelli@tuni.fi>
2022-11-30 15:14:58 +0200

    made some changes to logic

--- Group3/src/main/java/fi/tuni/swdesign/group3/view/ViewModel.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/view/ViewModel.java
@@ -32,78 +32,84 @@ public class ViewModel {
      */
     public RoadData[] onCalculateButtonPress(DataQuery query){
         RoadData[] toReturn = new RoadData[2];
-        if("Road data".equals(query.dataType)){
-            RoadTrafficData data = model.getRoadTrafficData(query.location);
-            
-            toReturn[0] = data;
-            return toReturn;
-            
-        } else if ("Weather data".equals(query.dataType)){
-            
-            String start = this.parseTime(query.timelineStart[1], query.timelineStart[0]);
-            String end = this.parseTime(query.timelineEnd[1], query.timelineEnd[0]);
-            
-            WeatherDataQuery wquery = (WeatherDataQuery)query;
-            
-            RoadWeatherData data;
-            
-            if(LocalDateTime.now(ZoneId.of("Europe/Helsinki")).toString().replace(".", ":").compareTo(start) < 0){
-                data = model.getRoadWeatherDataFuture(query.location, start, end);
-            } else {
-                data = model.getRoadWeatherDataPast(query.location, start, end);  
-            }
-            
-            if(!wquery.getSelectedPerMonthParams().isEmpty()){
-                String date = query.timelineStart[1].replace(".", "-");
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-                DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                String dateQuery = LocalDate.parse(date, formatter).format(formatter2); 
-                TreeMap<String, Float[]> avgs = model.getMonthlyAverages(query.location, dateQuery);
-                data.setMonthlyAverage(avgs);
+        if(null != query.dataType)switch (query.dataType) {
+            case "Road data" -> {
+                RoadTrafficData data = model.getRoadTrafficData(query.location);
                 
-                data.getMonthylAverage().forEach((key, value) -> {
-                    System.out.println(key+":"+Arrays.toString(value));
-                });
+                toReturn[0] = data;
+                return toReturn;
+                  
             }
-            
-            toReturn[0] = data;
-            return toReturn;
-        
-        } else if ("Combined data".equals(query.dataType)){
-            
-            CombinedDataQuery cbquery = (CombinedDataQuery) query;
-            
-            String start = this.parseTime(cbquery.timelineStart[1], cbquery.timelineStart[0]);
-            String end = this.parseTime(cbquery.timelineStart[1], cbquery.timelineEnd[0]);
-            
-            RoadTrafficData data = model.getRoadTrafficData(cbquery.location);
-            
-            RoadWeatherData data2;
-            
-            if(LocalDateTime.now(ZoneId.of("Europe/Helsinki")).toString().replace(".", ":").compareTo(start) < 0){
-                data2= model.getRoadWeatherDataFuture(cbquery.location, start, end);
-            } else {
-                data2 = model.getRoadWeatherDataPast(cbquery.location, start, end);
-            }
-            
-            if(!cbquery.getSubWeatherDQ().getSelectedPerMonthParams().isEmpty()){
-                String date = query.timelineStart[1].replace(".", "-");
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-                DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                String dateQuery = LocalDate.parse(date, formatter).format(formatter2); 
-                TreeMap<String, Float[]> avgs = model.getMonthlyAverages(query.location, dateQuery);
+            case "Weather data" -> {
                 
-                data2.setMonthlyAverage(avgs);
+                String start = this.parseTime(query.timelineStart[1], query.timelineStart[0]);
+                String end = this.parseTime(query.timelineEnd[1], query.timelineEnd[0]);
                 
-                data2.getMonthylAverage().forEach((key, value) -> {
-                    System.out.println(key+":"+Arrays.toString(value));
-                });
+                WeatherDataQuery wquery = (WeatherDataQuery)query;
+                
+                RoadWeatherData data;
+                
+                if(LocalDateTime.now(ZoneId.of("Europe/Helsinki")).toString().replace(".", ":").compareTo(start) < 0){
+                    data = model.getRoadWeatherDataFuture(query.location, start, end);
+                } else {
+                    data = model.getRoadWeatherDataPast(query.location, start, end);
+                }
+                
+                if(!wquery.getSelectedPerMonthParams().isEmpty()){
+                    String date = query.timelineStart[1].replace(".", "-");
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                    DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    String dateQuery = LocalDate.parse(date, formatter).format(formatter2);
+                    TreeMap<String, Float[]> avgs = model.getMonthlyAverages(query.location, dateQuery);
+                    data.setMonthlyAverage(avgs);
+                    
+                    data.getMonthylAverage().forEach((key, value) -> {
+                        System.out.println(key+":"+Arrays.toString(value));
+                    });
+                }
+                
+                toReturn[0] = data;
+                return toReturn;
                 
             }
-            
-            toReturn[0] = data;
-            toReturn[1] = data2;
-            return toReturn;
+            case "Combined data" -> {
+                
+                CombinedDataQuery cbquery = (CombinedDataQuery) query;
+                
+                String start = this.parseTime(cbquery.timelineStart[1], cbquery.timelineStart[0]);
+                String end = this.parseTime(cbquery.timelineStart[1], cbquery.timelineEnd[0]);
+                
+                RoadTrafficData data = model.getRoadTrafficData(cbquery.location);
+                
+                RoadWeatherData data2;
+                
+                if(LocalDateTime.now(ZoneId.of("Europe/Helsinki")).toString().replace(".", ":").compareTo(start) < 0){
+                    data2= model.getRoadWeatherDataFuture(cbquery.location, start, end);
+                } else {
+                    data2 = model.getRoadWeatherDataPast(cbquery.location, start, end);
+                }
+                
+                if(!cbquery.getSubWeatherDQ().getSelectedPerMonthParams().isEmpty()){
+                    String date = query.timelineStart[1].replace(".", "-");
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                    DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    String dateQuery = LocalDate.parse(date, formatter).format(formatter2);
+                    TreeMap<String, Float[]> avgs = model.getMonthlyAverages(query.location, dateQuery);
+                    
+                    data2.setMonthlyAverage(avgs);
+                    
+                    data2.getMonthylAverage().forEach((key, value) -> {
+                        System.out.println(key+":"+Arrays.toString(value));
+                    });
+                    
+                }
+                
+                toReturn[0] = data;
+                toReturn[1] = data2;
+                return toReturn;
+            }
+            default -> {
+            }
         }
         return null;
     }
