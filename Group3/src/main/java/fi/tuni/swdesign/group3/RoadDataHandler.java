704ca9ae2eccf484ec43f75d4a469bf81d11b51e
Aleksi Iso-Seppala <aleksi.iso-seppala@tuni.fi>
2022-11-30 14:57:40 +0200

    added boolean return for savePreference()

--- Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataHandler.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/RoadDataHandler.java
@@ -528,7 +528,7 @@ public class RoadDataHandler {
      * @param dataQuery, object containing the preferences
      * @throws IOException 
      */
-    public void savePreferences(DataQuery dataQuery) throws IOException{
+    public boolean savePreferences(DataQuery dataQuery) throws IOException{
         
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String fileName = "SavedPreferences.json";
@@ -639,6 +639,7 @@ public class RoadDataHandler {
         }
         gson.toJson(userPreferences,writer);
         writer.close();
+        return true;
     }
     
     /**
@@ -826,18 +827,14 @@ public class RoadDataHandler {
     
     public static void main(String args[]) throws Exception{
         
-        System.out.println("test");
         RoadDataHandler test = new RoadDataHandler();
-        RoadTrafficData roadData= test.fetchRoadData("Suomi");
-        
-        TreeMap<String, Float[]> monthlyData = test.fetchMonthlyAverages("Helsinki", "2022-11-30");
-                monthlyData.entrySet().forEach(entry -> {
-            System.out.println(entry.getKey() + " " + Arrays.toString(entry.getValue()));
-        });
+        RoadTrafficData roadData= test.fetchRoadData("Helsinki");
+        //RoadWeatherData weatherData = test.fetchWeatherDataPast("Helsiki", "2022-10-30T14:00:00Z", "2022-11-30T14:00:00Z");
+        test.saveDataBase(roadData, null, "test2");
+//        TreeMap<String, Float[]> monthlyData = test.fetchMonthlyAverages("Helsinki", "2022-11-30");
+//                monthlyData.entrySet().forEach(entry -> {
+//            System.out.println(entry.getKey() + " " + Arrays.toString(entry.getValue()));
+//        });
 
-        /* I'll store these here for a bit.
-        System.out.println(handler.fetchRoadData("Rovaniemi").getTemperature());
-        System.out.println(handler.fetchWeatherData("Rovaniemi", "2022-11-16T10:00:00Z", "2022-11-16T22:00:00Z").getTemperature());
-        */
     }
 }
