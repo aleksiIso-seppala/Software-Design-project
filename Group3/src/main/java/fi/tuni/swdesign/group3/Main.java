a02a061bb33b63595040954c6d5a2a2823d0522d
fqjupe <jukka.pelli@tuni.fi>
2022-12-01 13:02:15 +0200

    added some rant of an old geek to main

--- Group3/src/main/java/fi/tuni/swdesign/group3/Main.java
+++ Group3/src/main/java/fi/tuni/swdesign/group3/Main.java
@@ -1,16 +1,20 @@
 package fi.tuni.swdesign.group3;
 import fi.tuni.swdesign.group3.view.*;
+
 /**
- *
- * Placeholder for main
+ * One main to run them all, one main to find them, 
+ * One main to bring them all and in to RoadCast bind them.
+ * @author Amanda, Aleksi, Jukka Lauri
  */
 public class Main {
-    public static void main(String args[]) throws Exception{
+
+    /**
+     * main class to run it all
+     * @param args just to satisfy javagods
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception{
         MainView mainView = new MainView();
         mainView.initGUI();
-        /* I'll store these here for a bit.
-        System.out.println(handler.fetchRoadData("Rovaniemi").getTemperature());
-        System.out.println(handler.fetchWeatherData("Rovaniemi", "2022-11-16T10:00:00Z", "2022-11-16T22:00:00Z").getTemperature());
-        */
     }
 }
